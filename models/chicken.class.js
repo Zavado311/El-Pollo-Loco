4c3d06abd2d7b2b452f78f25329bebc1ababffd4
Zavado311 <brian.wigger@bluewin.ch>
2024-12-30 21:08:21 +0100

    Important functions

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,7 +1,28 @@
 class Chicken extends MovableObject {
-    constructor()  {
-        super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+  y = 325;
+  IMAGES_WALKING = [
+    "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+  ];
 
-        this.x = 200 + Math.random() * 500;
-       }
+  constructor() {
+    super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+    this.x = 300 + Math.random() * 500;
+    this.loadImages(this.IMAGES_WALKING);
+
+    this.speed = 0.15 + Math.random() * 0.5;
+
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.moveLeft();
+    }, 1000 / 60);
+
+    setInterval(() => {
+      this.playAnimation(this.IMAGES_WALKING);
+    }, 200);
+  }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -1,54 +1,101 @@
 class World {
-character = new Character();
-enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
+  character = new Character();
+  level = level1;
+  canvas;
+  ctx;
+  keyboard;
+  camera_x = 0;
+  statusBar = new StatusBar();
+  throwableObjects = [];
 
-    ];
-clouds = [
-    new Cloud()
-];
+  constructor(canvas, keyboard) {
+    this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
+    this.keyboard = keyboard;
+    this.draw();
+    this.setWorld();
+    this.run();
+  }
 
-backgroundObjects = [
-    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 80)
-]
-canvas;
-ctx;
+  setWorld() {
+    this.character.world = this;
+  }
 
-constructor(canvas) {
-this.ctx = canvas.getContext("2d");
-this.canvas = canvas;
-this.draw();
+  run() {
+    setInterval(() => {
+      this.checkCollisons();
+      this.checkThrowObjects();
+    }, 100);
+  }
 
-}
+  checkThrowObjects() {
+    if (this.keyboard.D) {
+      let bottle = new ThrowableObject(this.character.x + 100, this.character.y + 100);
+      this.throwableObjects.push(bottle);
+    }
+  }
 
-draw() {
-this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    
-    this.addObjectsToMap(this.backgroundObjects);
+  checkCollisons() {
+    this.level.enemies.forEach((enemy) => {
+      if (this.character.isColliding(enemy)) {
+        this.character.hit();
+        this.statusBar.setPercentage(this.character.energy);
+      }
+    });
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.ctx.translate(this.camera_x, 0);
+    this.addObjectsToMap(this.level.backgroundObjects);
+
+    this.ctx.translate(-this.camera_x, 0);
+    // ------ Space for fixed Objects ----------
+    this.addToMap(this.statusBar);
+    this.ctx.translate(this.camera_x, 0);
+
     this.addToMap(this.character);
-    this.addObjectsToMap(this.clouds);
-    this.addObjectsToMap(this.enemies);
-    
+    this.addObjectsToMap(this.level.clouds);
+    this.addObjectsToMap(this.level.enemies);
+    this.addObjectsToMap(this.throwableObjects);
+
+    this.ctx.translate(-this.camera_x, 0);
 
     // Draw() wird immer wieder aufgerufen
-    let self = this; 
-    requestAnimationFrame(function() {
-    self.draw();
+    let self = this;
+    requestAnimationFrame(function () {
+      self.draw();
     });
-        
-     }
+  }
 
-addObjectsToMap(objects) {
-   objects.forEach(o => {this.addToMap(o);
+  addObjectsToMap(objects) {
+    objects.forEach((o) => {
+      this.addToMap(o);
+    });
+  }
 
-   })
-}
+  addToMap(mo) {
+    if (mo.otherDirection) {
+      this.flipImage(mo);
+    }
+    mo.draw(this.ctx);
+    mo.drawFrame(this.ctx);
 
-addToMap(mo) {
-    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
-}
+    if (mo.otherDirection) {
+      this.flipImageBack(mo);
+    }
+  }
 
+  flipImage(mo) {
+    this.ctx.save();
+    this.ctx.translate(mo.width, 0);
+    this.ctx.scale(-1, 1);
+    mo.x = mo.x * -1;
+  }
 
+  flipImageBack(mo) {
+    mo.x = mo.x * -1;
+    this.ctx.restore();
+  }
 }
